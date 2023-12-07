1dde3e37c589aee14bbd470557a3a220246fb013
Mohamed Adel Elfeky <50705405+MohamedAdelElfeky@users.noreply.github.com>
2023-12-07 15:27:00 +0200

    delete order

--- database/migrations/2023_12_06_124359_create_carts_table.php
+++ database/migrations/2023_12_06_124359_create_carts_table.php
@@ -17,12 +17,12 @@ return new class extends Migration
             $table->unsignedBigInteger('product_id')->nullable();
             $table->integer('count');
             $table->integer('price');
-            $table->unsignedBigInteger('order_id')->nullable();
+            // $table->unsignedBigInteger('order_id')->nullable();
 
             // Foreign keys
             $table->foreign('user_id')->references('id')->on('users')->nullable();
             $table->foreign('product_id')->references('id')->on('products')->nullable();
-            $table->foreign('order_id')->references('id')->on('orders')->nullable();
+            // $table->foreign('order_id')->references('id')->on('orders')->nullable();
             $table->softDeletes();
             $table->timestamps();
         });
