5477ce87d30f77b95df625cd46c29a706e238097
Mohamed Adel Elfeky <50705405+MohamedAdelElfeky@users.noreply.github.com>
2023-12-12 16:52:07 +0200

    handle seeder

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,13 +15,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('nationality');
             $table->string('full_name');
             $table->string('email')->unique();
             $table->string('phone')->unique();
             $table->string('key_phone')->nullable();
             $table->string('password');
             $table->enum('sex', ['male', 'female'])->nullable();
+            $table->string('nationality')->nullable();
             $table->json('categories')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('avatar')->nullable();
